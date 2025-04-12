f50d27ef36f9cff73429e5b3a480e191806d4125
zacheus.eshitemi <zacheus.eshitemi@turnkeyafrica.com>
2025-04-12 15:32:44 +0300

    Add new code from tutorial video 11

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: false,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  menuType: String = 'default';
+  constructor(private route: Router) {}
 
+  ngOnInit(): void {
+    this.route.events.subscribe((val: any) => {
+      if (val.url) {
+        console.warn(val.url);
+        if (localStorage.getItem('seller') && val.url.includes('seller')) {
+          console.warn('this can only be accessed by the seller');
+          this.menuType = 'seller';
+        } else {
+          this.menuType = 'default';
+        }
+      }
+    });
+  }
 }
--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -10,6 +10,7 @@ import { signUp } from '../data-type';
 })
 export class SellerAuthComponent implements OnInit {
   showLogin = false;
+  authError:String='';
   constructor(private seller: SellerService) {}
 
   ngOnInit(): void {
@@ -22,7 +23,12 @@ export class SellerAuthComponent implements OnInit {
   }
 
   login(data: signUp): void {
-    console.warn(data);
+    this.seller.userLogin(data);
+    this.seller.isLoginError.subscribe((isError) => {
+      if(isError){
+        this.authError="Email or Password is Invalid";
+      }
+    });
   }
 
   openLogin() {
