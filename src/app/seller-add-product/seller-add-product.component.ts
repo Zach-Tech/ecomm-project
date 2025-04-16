71d6720951f2553f20726afbc90bac11402f0e90
zacheus.eshitemi <zacheus.eshitemi@turnkeyafrica.com>
2025-04-16 18:03:22 +0300

    Angular project tutorial #14 Add Product API Integration | Angular E-commerce Project

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-seller-add-product',
@@ -6,12 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './seller-add-product.component.html',
   styleUrl: './seller-add-product.component.css',
 })
-export class SellerAddProductComponent {
-  constructor() {}
+export class SellerAddProductComponent implements OnInit {
+  addProductMessage: string | undefined;
+  constructor(private product: ProductService) {}
 
   ngOnInit(): void {}
 
-  submit(data: object) {
-    console.warn(data);
+  submit(data: product) {
+    this.product.addProduct(data).subscribe((result) => {
+      console.warn(result);
+      if (result) {
+        this.addProductMessage = 'Product is added successfully';
+      }
+    });
+
+    setTimeout(() => {
+      this.addProductMessage = undefined;
+    }, 3000);
   }
 }
