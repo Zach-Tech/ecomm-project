e0b2d553e4c81412f4461e381bbb2a826003f429
zacheus.eshitemi <zacheus.eshitemi@turnkeyafrica.com>
2025-04-27 11:10:36 +0300

    Angular project tutorial #42 Cart Page UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { SearchComponent } from './search/search.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { UserAuthComponent } from './user-auth/user-auth.component';
 import { FooterComponent } from './footer/footer.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { FooterComponent } from './footer/footer.component';
     ProductDetailsComponent,
     UserAuthComponent,
     FooterComponent,
+    CartPageComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, FontAwesomeModule, NgbModule, NgbCarouselModule],
   providers: [
