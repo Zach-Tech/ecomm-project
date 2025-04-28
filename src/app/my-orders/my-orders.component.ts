7c058586142928e94095bd675cb2231b60cf277a
zacheus.eshitemi <zacheus.eshitemi@turnkeyafrica.com>
2025-04-28 10:38:11 +0300

    Angular project tutorial #48 Cancel Order API

--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -14,15 +14,21 @@ export class MyOrdersComponent implements OnInit {
   constructor(private product: ProductService) {}
 
   ngOnInit(): void {
-    // this.getOrderList();
+    this.getOrderList();
+  }
+
+  cancelOrder(orderId: number | undefined) {
+    orderId &&
+      this.product.cancelOrder(orderId).subscribe((result) => {
+        if (result) {
+          this.getOrderList();
+        }
+      });
+  }
+
+  getOrderList() {
     this.product.orderList().subscribe((result) => {
       this.orderData = result;
     });
   }
-
-  // getOrderList() {
-  //   this.product.orderList().subscribe((result) => {
-  //     this.orderData = result;
-  //   });
-  // }
 }
